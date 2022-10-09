d962623aadf988d76f79c485fb3be69d17dc9724
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-10-09 15:04:34 +0530

    updated button content

--- screens/Signup.js
+++ screens/Signup.js
@@ -3,10 +3,35 @@ import React from 'react'
 
 export default function Signup() {
     return (
-        <View>
-          <Text style={{color:'red' , fontSize:50 , position:'absolute' ,right :'30%'}}>hellow</Text>
+        <View style={styles.container}>
+          <Text style={{color:'#f6c708' , fontSize:50 , position:'absolute' ,right :'30%'}}>Sign up</Text>
+
+          <TextInput
+          placeholderTextColor="black"
+          style={styles.input1}
+          placeholder="Username"
+        />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      height:'100%',
+      backgroundColor: '#181718',
+    },
+    input1: {
+      marginTop: '50%',
+      borderWidth: 1,
+      padding: 10,
+      width: '80%',
+      color: '#000000',
+      backgroundColor:'#ffffff',
+      borderRadius: 10,
+    }
+
+});
 
 
