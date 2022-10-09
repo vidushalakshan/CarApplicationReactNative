696300ed5ae8c4a801693f599de1da6b2b5b5327
Vidusha Lakshan <90233619+vidushalakshan@users.noreply.github.com>
2022-10-09 15:08:50 +0530

    updated content

--- screens/Signup.js
+++ screens/Signup.js
@@ -4,32 +4,50 @@ import React from 'react'
 export default function Signup() {
     return (
         <View style={styles.container}>
-          <Text style={{color:'#f6c708' , fontSize:50 , position:'absolute' ,right :'30%'}}>Sign up</Text>
+            <Text style={{ color: '#f6c708', fontSize: 50, position: 'absolute' }}>Sign up</Text>
 
-          <TextInput
-          placeholderTextColor="black"
-          style={styles.input1}
-          placeholder="Username"
-        />
+            <TextInput 
+                placeholderTextColor="black"
+                style={styles.input1}
+                placeholder="Username"
+            />
+
+            <TextInput
+                placeholderTextColor="black"
+                style={styles.input1}
+                placeholder="Username"
+            />
+
+
+            <TextInput
+                placeholderTextColor="black"
+                style={styles.input1}
+                placeholder="Username"
+            />
+
+            <TextInput
+                placeholderTextColor="black"
+                style={styles.input1}
+                placeholder="Username"
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-      justifyContent: 'center',
-      alignItems: 'center',
-      height:'100%',
-      backgroundColor: '#181718',
+        alignItems: 'center',
+        height: '100%',
+        backgroundColor: '#181718',
     },
     input1: {
-      marginTop: '50%',
-      borderWidth: 1,
-      padding: 10,
-      width: '80%',
-      color: '#000000',
-      backgroundColor:'#ffffff',
-      borderRadius: 10,
+        marginTop: '22%',
+        borderWidth: 1,
+        padding: 10,
+        width: '80%',
+        color: '#000000',
+        backgroundColor: '#ffffff',
+        borderRadius: 10,
     }
 
 });
